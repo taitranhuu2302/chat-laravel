e89a4a87b7197cb6cd7c9c332e94d000c3adc2e6
taitranhuu1908 <emlacuaanh1908@gmail.com>
2022-04-04 17:51:49 +0700

    config database

---
+++ database/migrations/2022_04_04_102700_create_rooms_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Room::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->boolean('owner')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms_users');
+    }
+};
