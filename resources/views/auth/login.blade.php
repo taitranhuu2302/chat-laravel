05a56c8b476a3d3bc7172d1f612c3cfe41032f18
taitranhuu1908 <emlacuaanh1908@gmail.com>
2022-04-14 03:11:20 +0700

    feat: login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <title>Login Chat App</title>
+</head>
+<body>
+<div id="wrapper">
+    <div class="grid grid-cols-12 h-full">
+        <div class="col-span-4">1</div>
+        <div class="col-span-8 h-full bg-white content flex flex-col">
+            <div class="mx-auto mb-auto">
+                <div class="flex flex-col gap-5 items-center">
+                    <h1 class="text-2xl font-semibold">Welcome Back !</h1>
+                    <p class="text-lg text-gray-500">Sign in to continue to Doot</p>
+                </div>
+            </div>
+            <div>d</div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
