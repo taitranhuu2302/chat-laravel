d1903c00b74e65ff7029d345df87589cffedebde
taitranhuu1908 <emlacuaanh1908@gmail.com>
2022-05-23 09:30:05 +0700

    feat: Sidebar Todo

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -52,6 +52,21 @@
                     <div class="indicator__inner"></div>
                 </div>
             </li>
+            <li id="nav__request" class="relative">
+                <button id="button-friend-request" data-tabs-target="#todo" type="button" role="tab"
+                    aria-controls="friend-request" aria-selected="false" data-tooltip-target="tooltip-todo"
+                    class="nav__top--button">
+                    <i class="nav__top--icon far fa-check-square"></i>
+                    <div id="tooltip-todo" role="tooltip"
+                        class="inline-block absolute whitespace-nowrap invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                        Công việc cần làm
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                </button>
+                <div class="indicator__dot hidden">
+                    <div class="indicator__inner"></div>
+                </div>
+            </li>
         </ul>
         <ul class="nav__bottom">
             <li>
@@ -90,6 +105,9 @@
         </div>
         <div id="friend-request" class="hidden" role="tabpanel" aria-labelledby="button-friend-request">
             <x-sidebar-friend-request :friendRequests="$friendRequests"></x-sidebar-friend-request>
+        </div>
+        <div id="todo">
+            <x-sidebar-todo></x-sidebar-todo>
         </div>
     </div>
 </div>
